2c9e2b4b0eb80eceb59f5c3028c7f9d00f3f2386
Jonathan PHILIPPE <jphilippe@fluence.eu>
2025-12-08 14:19:07 +0100

    Initial commit

---
+++ lib/money/exchange.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "exchange/version"
+
+module Money
+  module Exchange
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
